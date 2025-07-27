51ca3130b152277a5413eade29194d62cb80dd0e
tahaserdarmete <tserdarmete@gmail.com>
2025-07-27 04:11:24 +0300

    garden of flavor detailpage,render,not found section ve garden of flavor bitiş

--- js/main.js
+++ js/main.js
@@ -1,16 +1,63 @@
 import getMenu from "./api.js";
-import {renderMenuCard} from "./ui.js";
+import {
+  renderDetailPage,
+  renderLoader,
+  renderMenuCard,
+  renderNotFound,
+  uiElements,
+} from "./ui.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
+  // Loader'ı render et
+  renderLoader();
   // Api isteği at
   const menuData = await getMenu();
 
   // Hangi sayfada olduğumuza karar ver. Anasayfaysak buna göre işlemler detay sayfasındaysak buna göre işlemler yapacağız.
   if (window.location.pathname.includes("/index.html")) {
-    console.log("Ana Sayfa");
     // menu elemanlarını dinamik şekilde render et
     renderMenuCard(menuData);
+
+    // uiElements.categoryButtons bir nodeList olduğundan buna addEventListener eklyemeyiz. Bunun için nodeList içerisindeki her bir elemanı teker teker erişip addEventListener ekleyeceğiz.
+    uiElements.categoryButtons.forEach((button) => {
+      button.addEventListener("click", () => {
+        // Butona tıklanınca butonun ID'sine eriş
+        const selectedCategory = button.id;
+
+        // menuData içerisindeki selected Category sahip elemanlara eriş
+
+        const filteredMenu = menuData.filter(
+          (item) => item.category == selectedCategory
+        );
+
+        console.log(filteredMenu);
+
+        // Filtrelenen ürünlere göre menü listesini renderla. Eğer seçili kategori all' a eşitse tüm ürünleri renderla ama all haricinde bir değere eşitse o kategorideki ürünleri renderla.
+
+        if (selectedCategory == "all") {
+          renderMenuCard(menuData);
+        } else {
+          renderMenuCard(filteredMenu);
+        }
+      });
+    });
   } else {
-    console.log("Detay Sayfası");
+    // Url'deki parametreye eriş
+    // ! UrlSearchParams Js içerisinde yer alan bir classtır. Url'e geçilen parametreleri kolay bir şekilde yönetmemizi sağlar.
+    const params = new URLSearchParams(window.location.search);
+
+    // Ürünün ID'sini number veri tipine dönüştür ve bir değişkene aktar.
+    const itemId = Number(params.get("id"));
+
+    //menuData içerisinde itemID'ye sahip elemanı bul
+    const product = menuData.find((item) => item.id == itemId);
+
+    // Eğer product yoksa not-found sayfasını renderla
+    if (!product) {
+      renderNotFound();
+    } else {
+      // Bulunan product' a göre arayüzü renderla
+      renderDetailPage(product);
+    }
   }
 });
--- js/ui.js
+++ js/ui.js
@@ -1,12 +1,13 @@
 // Arayüzden eleman çekme işlmini bir obje içerisinde yap
 const uiElements = {
   menuList: document.querySelector("#menu-list"),
+  categoryButtons: document.querySelectorAll(".category-botton"),
+  detailContainer: document.querySelector("#detail-container"),
 };
 
 // Menu elemanı render edecek fonksiyon
 
 const renderMenuCard = (data) => {
-  console.log(data);
   // Dışarıdan alınan datanın içerisindeki her eleman için bir html oluştur.
   uiElements.menuList.innerHTML = data
     .map(
@@ -42,4 +43,91 @@ const renderMenuCard = (data) => {
   // renderMenuCard fonksiyonunu verilen data bir dizidir. Bu dizinin dönülmesi ile elde edilen elemansa yine bir dizidir. Bu elde edilen dizi arayüze aktarıldığında elemanlar arasındaki "," arayüzde bozulmaya sebep olur. Çünkü dizi elemanları "," ile ayrılır. Bu bozulmayı düzeltmek içinse dizinin elemanları join metodu kullanılarak boşluğa göre böleriz.
 };
 
-export {uiElements, renderMenuCard};
+// Detail Container' ın içeriğini dinamik şekilde renderlayacak fonksiyon
+const renderDetailPage = (product) => {
+  console.log(product);
+  // uiElement içerisindeki detailcontainer' ın html içeriğini belirler
+
+  uiElements.detailContainer.innerHTML = `      <section class="container my-5 shadow" style="max-width: 1100px">
+        
+        <div
+          class="d-flex align-items-center justify-content-between mt-5 gap-2 py-3 px-2"
+        >
+          <a href="/index.html" class="text-success fs-4"
+            ><i class="bi bi-house-fill"></i
+          ></a>
+
+          <div class="detail-breadcrumb ms-2">
+            <span class="text-muted">detail /</span>
+            <span class="text-muted"> ${product.category} /</span>
+            <span> ${product.title}</span>
+          </div>
+        </div>
+        
+        <div class="row p-5 g-5 align-items-center">
+          <div class="col-12 col-md-5 mb-4">
+            <img
+              src="${product.img}"
+              alt="detail-image"
+              class="img-fluid rounded-4 w-100 shadow detail-img"
+            />
+          </div>
+          <div class="col-12 col-md-7 mb-4">
+            <h2 class="fw-bold text-success mb-3">${product.title}</h2>
+            <div class="mb-3">
+              <span
+                class="badge bg-warning text-dark fs-6 px-3 py-2 text-capitalize me-2"
+                >${product.category}</span
+              >
+              <span
+                class="badge bg-light text-success fs-6 px-3 py-2 text-capitalize"
+                >$${product.price.toFixed(2)}</span
+              >
+            </div>
+            <p class="lead text-muted mb-4">
+             ${product.desc}
+            </p>
+
+            <button class="btn btn-success shadow px-4 py-2 fw-semibold">
+              Order Now
+            </button>
+          </div>
+        </div>
+      </section>`;
+};
+
+// Ürün bulunamadığında ürün bulunamadığı içeriği renderlanacak fonksiyon
+
+const renderNotFound = () => {
+  uiElements.detailContainer.innerHTML = ` 
+  <section class="container my-5" style="max-width:800px">
+  <div class="text-center">
+
+  <img src="https://cdn-icons-png.flaticon.com/512/2748/2748558.png" class="img-fluid mb-4"/>
+  <h2 class="text-danger fw-bold fs-1">Product not founded !</h2>
+  <p class="text-muted mb-4 lead">The product you are looking is not avaible !</p>
+  <a href="/index.html" class="btn btn-danger px-3 py-2 shadow fw-bold">Go to home page</a>
+  </div>
+  </section>
+  `;
+};
+
+// Loader render edecek fonksiyon
+
+const renderLoader = () => {
+  uiElements.menuList.innerHTML = `
+  <div class="d-flex justify-content-center align-items-center w-100 py-5">
+  <div class="spinner-border text-success" role="status">
+  <span class="visually-hidden">Loading...</span>
+  </div>
+  </div>
+  `;
+};
+
+export {
+  uiElements,
+  renderMenuCard,
+  renderDetailPage,
+  renderNotFound,
+  renderLoader,
+};
